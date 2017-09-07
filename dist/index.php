4e43447abc1010bcf25b4b3e3f8f96d2f6703546
Jean-Christophe Sibon <jcsibon@gmail.com>
2017-09-07 12:39:09 +0200

    first commit

---
+++ dist/index.php
@@ -0,0 +1,36 @@
+<?php
+
+require('../vendor/autoload.php');
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+// Register view rendering
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Our web handlers
+
+$app->get('/', function() use($app) {
+  return $app['twig']->render('index.html');
+});
+
+$app->get('/manifesto', function() use($app) {
+  return $app['twig']->render('manifesto.html');
+});
+
+$app->get('/usines', function() use($app) {
+  return $app['twig']->render('usines.html');
+});
+
+$app->get('/histoire', function() use($app) {
+  return $app['twig']->render('histoire.html');
+});
+
+$app->run();
