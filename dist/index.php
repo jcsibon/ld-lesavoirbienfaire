b22c02e202a2633212f8c4c44e59ff199b5ec60d
Jean-Christophe Sibon <jcsibon@gmail.com>
2017-09-07 12:48:13 +0200

    fixing folders structure

--- dist/index.php
+++ dist/index.php
@@ -12,7 +12,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/public',
+    'twig.path' => __DIR__.'/views',
 ));
 
 // Our web handlers
